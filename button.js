f93cd89eef890dde8e1cf0921ef4b9b1875d0ebe
Adam Walsh <111248733+amwalsh965@users.noreply.github.com>
2023-02-05 17:40:10 -0500

    backup

--- button.js
+++ button.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	
+	//lots of different functions for the buttons on the screen
 	$(".options").click(function() {
 		$(".option-dropdown").toggleClass("dropdown-open-click", true);
 		$(".settings").toggleClass("dropdown-opened", true);
@@ -24,71 +24,73 @@ $(document).ready(function(){
 	
 	$(".player1").click(function() {
 		$(".player1").toggleClass("activated", true);
-		$(".player2, .player3, .player4, .player5, .player6, .player7, .player8, .player9, .player10").css("visibility", "hidden");
 	});
 	
 	$(".player2").click(function() {
 		$(".player2").toggleClass("activated", true); 
-		$(".player1, .player3, .player4, .player5, .player6, .player7, .player8, .player9, .player10").css("visibility", "hidden");
 	});
 	
 	$(".player3").click(function() {
 		$(".player3").toggleClass("activated", true); 
-		$(".player2, .player1, .player4, .player5, .player6, .player7, .player8, .player9, .player10").css("visibility", "hidden");
 	});
 	
 	$(".player4").click(function() {
 		$(".player4").toggleClass("activated", true); 
-		$(".player2, .player3, .player1, .player5, .player6, .player7, .player8, .player9, .player10").css("visibility", "hidden");
+
 	});
 	
 	$(".player5").click(function() {
 		$(".player5").toggleClass("activated", true); 
-		$(".player2, .player3, .player4, .player1, .player6, .player7, .player8, .player9, .player10").css("visibility", "hidden");
+
 	});
 	
 	$(".player6").click(function() {
 		$(".player6").toggleClass("activated", true); 
-		$(".player2, .player3, .player4, .player5, .player1, .player7, .player8, .player9, .player10").css("visibility", "hidden");
 	});
 	
 	$(".player7").click(function() {
 		$(".player7").toggleClass("activated", true); 
-		$(".player2, .player3, .player4, .player5, .player6, .player1, .player8, .player9, .player10").css("visibility", "hidden");
 	});
 	
 	$(".player8").click(function() {
 		$(".player8").toggleClass("activated", true); 
-		$(".player2, .player3, .player4, .player5, .player6, .player7, .player1, .player9, .player10").css("visibility", "hidden");
 	});
 	
 	$(".player9").click(function() {
 		$(".player9").toggleClass("activated", true); 
-		$(".player2, .player3, .player4, .player5, .player6, .player7, .player8, .player1, .player10").css("visibility", "hidden");
 	});
 	
 	$(".player10").click(function() {
 		$(".player10").toggleClass("activated", true); 
-		$(".player2, .player3, .player4, .player5, .player6, .player7, .player8, .player9, .player1").css("visibility", "hidden");
 	});
 	
 	$(".create-game-settings-button1").click(function() {
 		var text = $(".create-game-settings-button1").html();
 		$(".main-settings-heading").text(text);
+		$(".game-info-settings").css("display", "block");
+		$(".game-settings-settings").css("display", "none");
+		$(".special-settings").css("display", "none");
 	});
 	
 	$(".create-game-settings-button2").click(function() {
 		var text = $(".create-game-settings-button2").html();
 		$(".main-settings-heading").text(text);
+		$(".game-info-settings").css("display", "none");
+		$(".game-settings-settings").css("display", "block");
+		$(".special-settings").css("display", "none");
 	});
 	
 	$(".create-game-settings-button3").click(function() {
 		var text = $(".create-game-settings-button3").html();
 		$(".main-settings-heading").text(text);
+		$(".game-info-settings").css("display", "none");
+		$(".game-settings-settings").css("display", "none");
+		$(".special-settings").css("display", "block");
 	});
 	
 	
-	
-	
+	//$(".update-rules").click(function() {
+	//	window.location.href = "http://localhost:8080/PokerServlet/PokerCreate.jsp";
+	//});
 	
 });
