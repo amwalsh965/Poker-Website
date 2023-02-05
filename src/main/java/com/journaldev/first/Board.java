73390a120ec2d2a20d6f3d23fff69e005d510ddf
Adam Walsh <111248733+amwalsh965@users.noreply.github.com>
2023-02-05 18:06:47 -0500

    Update Board.java

--- src/main/java/com/journaldev/first/Board.java
+++ src/main/java/com/journaldev/first/Board.java
@@ -2,9 +2,8 @@ package com.journaldev.first;
 import java.util.*;
 
 public class Board {
-   private ArrayList<String> boardCards = new ArrayList<String>();
+   public ArrayList<String> boardCards = new ArrayList<String>();
    public ArrayList<String> deck;
-   public ArrayList<String> emptyArray = new ArrayList<String>();
    
    public static int cardsInPlay = 0;
    private static int arrayNum = 0;
@@ -14,35 +13,7 @@ public class Board {
    public Board() {
       deck = new ArrayList<String>();
    }
-   
-   public static void main( String args[]) {
-   
-   }
-   
-//methods for board creation 
-      
-   //creates the board at different stages in the game identified with c
-   public void board(int c) {  
-      burnCard('_');
-      makeShape('_', c, this);
-      burnCard('|');
-      makeShape('?', c, this);
-      burnCard('|');
-      makeShape('|', c, this);
-      burnCard('!');
-      makeShape('!', c, this);
-      System.out.println("\n");
-   }
-   
-   //prints an empty board to the console
-   public void emptyBoard() {
-      burnCard('_');
-      burnCard('|');
-      burnCard('|');
-      burnCard('!');
-      System.out.println();
-   }
-   
+  
    //card creation
 
       //Creates cards in each suit        
@@ -64,64 +35,23 @@ public class Board {
    }
    
       //prints all cards in the deck
-   public void printCards() {
+  /* public void printCards() {
       System.out.println(this.deck);
    }
+   */
       
       //resets the deck, the used cards arrayList, and the board
    public void resetAll() {
       this.createAllCards();
-      this.emptyBoard();
+      cardsInPlay = 0;
+      this.boardCards.clear();
+      //this.emptyBoard();
    }
 
 //game logic
    //picks the next card for the board. Is used three cards for the flop. After card is picked, it is deleted from the temporary ArrayList************************************
       
    //picks the cards that will be used in the game
-   public void pickCardsForGame(int c) {
-         if (c == 3) {
-             for(int i = 0; i <= 2; i++) {
-                  pickCards(this);
-              } 
-         } else if (c == 4) {
-            pickCards(this);
-         } else if (c == 5) {
-            pickCards(this);
-         } else {
-            System.out.println("invalid Entry");
-         }
-   }    
-   
-   //returns a specific card on the board
-   public String getBoardCards(int i) { 
-    if (i == 1) {
-      return boardCards.get(0);
-    } else if (i == 2) {
-      return boardCards.get(1);
-    } else if (i == 3) {
-      return boardCards.get(2);
-    } else if (i == 4) {
-      return boardCards.get(3);
-    } else if (i == 5) {
-      return boardCards.get(4);
-    } else {
-      System.out.print("Invalid Entry");
-    }
-    return "0";
-   }
-   
-   //returns all of the cards on the board in an arraylist
-   public ArrayList<String> getAllIntBoardCards() {
-   return boardCards;
-   }
-   
-   public void setBoard(String a, String b, String c, String d, String e) {
-      this.boardCards.add(a);
-      this.boardCards.add(b);
-      this.boardCards.add(c);
-      this.boardCards.add(d);
-      this.boardCards.add(e);
-   }
    
    private static void pickCards(Board b1) {
       arrayNum = (int)(Math.random() * (52 - cardsInPlay));
@@ -138,6 +68,48 @@ public class Board {
       cardsInPlay += 1;
    }
    
+   public void pickCardsForGame() {
+       for(int i = 1; i <= 25; i++) {
+            pickCards(this);
+       } 
+   }
+   
+   /* public void pickCardsForGame(int c) {
+         if (c == 3) {
+             for(int i = 0; i <= 2; i++) {
+                  pickCards(this);
+              } 
+         } else if (c == 4) {
+            pickCards(this);
+         } else if (c == 5) {
+            pickCards(this);
+         } else {
+            System.out.println("invalid Entry");
+         }
+   }    
+   */
+   //returns a specific card on the board
+   public String getBoardCards(int i) { 
+    return boardCards.get(i-1);
+   }
+   
+   //returns all of the cards on the board in an arraylist
+   public ArrayList<String> getAllBoardCards() {
+	   ArrayList<String> trueBoardCards = new ArrayList<String>();
+	   for(int i = 0; i < 5; i++) {
+		   trueBoardCards.add(boardCards.get(i));
+	   }
+	   return trueBoardCards;
+   }
+   
+   /*public void setBoard(String a, String b, String c, String d, String e) {
+      this.boardCards.add(a);
+      this.boardCards.add(b);
+      this.boardCards.add(c);
+      this.boardCards.add(d);
+      this.boardCards.add(e);
+   } */
+   
    private static ArrayList<String> createCardsForSuit(char s) {
       ArrayList<String> temp = new ArrayList<String>();
    
@@ -147,6 +119,83 @@ public class Board {
       return temp;
    }
    
+   public ArrayList<String> convertCard() {
+	   ArrayList<String> temp = new ArrayList<String>();
+	   String tempString = "";
+	   for(int i = 1; i <= ((PokerServlet.numBots * 2)+5); i++) {
+		   if(this.getBoardCards(i).length() == 3) {
+			   tempString += this.getBoardCards(i).substring(0, 2);
+		   } else {
+			   if(this.getBoardCards(i).substring(0, 1).equals("1")) {
+				   tempString += "ace";
+			   } else if(this.getBoardCards(i).substring(0, 1).equals("J")) {
+				   tempString += "jack";
+			   } else if(this.getBoardCards(i).substring(0, 1).equals("Q")) {
+				   tempString += "queen";
+			   } else if(this.getBoardCards(i).substring(0, 1).equals("K")) {
+				   tempString += "king";
+			   } else {
+			   tempString += this.getBoardCards(i).substring(0, 1);
+			   }
+		   }
+		   
+		   tempString += "_of_";
+		   
+		   if(this.getBoardCards(i).length() == 3) {
+			   if(this.getBoardCards(i).substring(2, 3).equals("h")) {
+				   tempString += "hearts";
+			   } else if(this.getBoardCards(i).substring(2, 3).equals("s")) {
+				   tempString += "spades";
+			   } else if(this.getBoardCards(i).substring(2, 3).equals("c")) {
+				   tempString += "clubs";
+			   } else if(this.getBoardCards(i).substring(2, 3).equals("d")) {
+				   tempString += "diamonds";
+			   }
+		   } else if(this.getBoardCards(i).length() == 2){
+			   if(this.getBoardCards(i).substring(1, 2).equals("h")) {
+				   tempString += "hearts";
+			   } else if(this.getBoardCards(i).substring(1, 2).equals("s")) {
+				   tempString += "spades";
+			   } else if(this.getBoardCards(i).substring(1, 2).equals("c")) {
+				   tempString += "clubs";
+			   } else if(this.getBoardCards(i).substring(1, 2).equals("d")) {
+				   tempString += "diamonds";
+			   }
+		   }
+		   tempString += ".png";
+		   tempString = "images/" + tempString;
+		   temp.add(tempString);
+		   tempString = "";
+	   }
+	   return temp;
+   }
+   
+ //methods for board creation 
+   
+   //creates the board at different stages in the game identified with c
+  /* public void board(int c) {  
+      burnCard('_');
+      makeShape('_', c, this);
+      burnCard('|');
+      makeShape('?', c, this);
+      burnCard('|');
+      makeShape('|', c, this);
+      burnCard('!');
+      makeShape('!', c, this);
+      System.out.println("\n");
+   } */
+   
+   //prints an empty board to the console
+  /*
+   public void emptyBoard() {
+      burnCard('_');
+      burnCard('|');
+      burnCard('|');
+      burnCard('!');
+      System.out.println();
+   }
+   */
+ /*  
    private static void makeShape(char a, int c, Board b1) {
       if (a == '_') {
          for(int i = 1; i <= c; i++) {
@@ -178,7 +227,8 @@ public class Board {
          }
       }
    }
-
+*/
+  /*
    private static void burnCard(char a) {
       if (a == '_') {
          System.out.print("\t  __      ");
@@ -188,4 +238,5 @@ public class Board {
          System.out.print("\n\t| __ |    ");
       }
    }
+   */
  }
